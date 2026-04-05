f89e2badff11d9aa151fd74326aee8f6430a6602
drewondigital <drewondigital@gmail.com>
2026-04-04 22:27:55 -0500

    Automated commit 2026-04-04 22:27:55

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 # go to your repo
-cd ~/drewondigital/homelab_infrastructure
+
+# cd /path/to/your/repo
 
 # make sure we are on main (or your branch)
 git checkout master
+echo "Checked out to master branch"
+echo " "
+sleep 2
 
 # add changes
 git add .
+echo "Added changes to staging area"
+echo " "
+sleep 2
 
 # commit
 git commit -m "Automated commit $(date +'%Y-%m-%d %H:%M:%S')"
+echo " "
+sleep 2
+
+echo "Committed changes with message: Automated commit $(date +'%Y-%m-%d %H:%M:%S')"
+echo " "
+sleep 2
+
 git commit -m "Changed file(s): $(git diff --name-only)"
+echo "Committed changes with message: Changed file(s): $(git diff --name-only)"
+echo " "
+sleep 2
 
 # push
 git push origin master
