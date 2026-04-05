d26257bdefa4e76a253e6a087f49916df33e61bd
drewondigital <drewondigital@gmail.com>
2026-04-04 22:34:47 -0500

    Automated commit 2026-04-04 22:34:47

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -6,31 +6,22 @@
 # make sure we are on main (or your branch)
 git checkout master
 echo "Checked out to master branch"
-echo " "
 sleep 7
 
 # add changes
 git add .
 echo "Added changes to staging area"
-echo " "
-echo " "
 sleep 7
 
 # commit
 git commit -m "Automated commit $(date +'%Y-%m-%d %H:%M:%S')"
-echo " "
-echo " "
 sleep 7
 
 echo "Committed changes with message: Automated commit $(date +'%Y-%m-%d %H:%M:%S')"
-echo " "
-echo " "
 sleep 7
 
 git commit -m "Changed file(s): $(git diff --name-only)"
 echo "Committed changes with message: Changed file(s): $(git diff --name-only)"
-echo " "
-echo " "
 sleep 7
 
 # push
