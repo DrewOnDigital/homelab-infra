521973343a2669fafce355af30a7bcef1b852a34
drewondigital <drewondigital@gmail.com>
2026-04-05 03:27:32 -0500

    Automated commit 2026-04-05 03:27:32

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -18,12 +18,12 @@ sleep 3
 echo " "
 echo " "
 echo "$(git status)"
-sleep 7
+sleep 2
 echo " "
 echo " "
 echo " "
 echo "Please abort this script NOW if you do not want to commit the aforementioned changes! Otherwise, the script will continue and these changes will be added to the commit."
-sleep 7
+sleep 2
 echo " "
 echo " "
 echo " "
@@ -33,23 +33,23 @@ echo " "
 # add changes we want to stage
 git add .
 echo "Aformentioned changes have been staged for commit."
-sleep 7
+sleep 2
 echo " "
 echo " "
 echo " "
 
 # commit
 git commit -m "Automated commit $(date +'%Y-%m-%d %H:%M:%S')"
-sleep 7
+sleep 2
 echo " "
-sleep 7
+sleep 2
 echo " "
 echo " "
 echo " "
 echo " "
 
 git commit -m "Changed file(s): $(git diff --name-only)"
-sleep 7
+sleep 2
 echo " "
 echo " "
 echo " "
@@ -57,8 +57,8 @@ echo " "
 
 
 # push
-git push 
-sleep 7
+git push -u origin master
+sleep 2
 echo " "
 echo " "
 echo "Pushed changes to remote repository"
