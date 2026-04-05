67cdda368b53bf8c7e5b06ad915cf7d9d11a8685
drewondigital <drewondigital@gmail.com>
2026-04-04 22:19:10 -0500

    dev

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # go to your repo
-cd /path/to/repo
+cd ~/drewondigital/homelab_infrastructure
 
 # make sure we are on main (or your branch)
-git checkout main
+git checkout master
 
 # add changes
 git add .
 
 # commit
 git commit -m "Automated commit $(date +'%Y-%m-%d %H:%M:%S')"
+git commit -m "Changed file(s): $(git diff --name-only)"
 
 # push
-git push origin main
+git push origin master
